3fea3f1ead352ab4945fb366c653edd0fb4a8a5f
newmoon93 <seongwon93@gmail.com>
2020-12-27 13:30:52 +0000

    greedy practice 2

---
+++ 03_그리디_거스름돈.cpp
@@ -0,0 +1,19 @@
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int n = 0;
+// int cnt = 0;
+// int coinTypes[4] = {500, 100, 50, 10};
+
+// int main()
+// {
+//   cout << "받은 돈 :";
+//   cin >> n;
+//   for (int i = 0; i < 4; i++) {
+//     int coin = coinTypes[i];
+//     cnt += n / coin;
+//     n %= coin;
+//   }
+//   cout << cnt << endl;
+// }
---
+++ 03_그리디_큰_수의_법칙.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m, k;
+vector<int> v;  // 컨테이너 생성
+
+int main()
+{
+  cin >> n >> m >> k;
+
+  for (int i = 0; i < n; i ++) {
+    int x;
+    cin >> x;
+    v.push_back(x); // 요소 추가
+  }
+
+  sort(v.begin(), v.end());
+  int first = v[n - 1];
+  int second = v[n - 2];
+
+  int cnt = (m / (k + 1)) * k;  //
+  cnt += m % (k + 1);           //
+
+  int result = 0;
+  result += cnt * first;
+  result += (m - cnt) * second;
+
+  cout << result << endl;
+}
