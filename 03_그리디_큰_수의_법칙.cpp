f5b8a32dd7d527cf4906c6f0bb65087d581fee76
newmoon93 <seongwon93@gmail.com>
2020-12-27 14:34:23 +0000

    greedy parctice 3

--- 03_그리디_큰_수의_법칙.cpp
+++ 03_그리디_큰_수의_법칙.cpp
@@ -1,30 +1,30 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
 
-using namespace std;
+// using namespace std;
 
-int n, m, k;
-vector<int> v;  // 컨테이너 생성
+// int n, m, k;
+// vector<int> v;  // 컨테이너 생성
 
-int main()
-{
-  cin >> n >> m >> k;
+// int main()
+// {
+//   cin >> n >> m >> k;
 
-  for (int i = 0; i < n; i ++) {
-    int x;
-    cin >> x;
-    v.push_back(x); // 요소 추가
-  }
+//   for (int i = 0; i < n; i ++) {
+//     int x;
+//     cin >> x;
+//     v.push_back(x); // 요소 추가
+//   }
 
-  sort(v.begin(), v.end());
-  int first = v[n - 1];
-  int second = v[n - 2];
+//   sort(v.begin(), v.end());
+//   int first = v[n - 1];
+//   int second = v[n - 2];
 
-  int cnt = (m / (k + 1)) * k;  //
-  cnt += m % (k + 1);           //
+//   int cnt = (m / (k + 1)) * k;  //
+//   cnt += m % (k + 1);           //
 
-  int result = 0;
-  result += cnt * first;
-  result += (m - cnt) * second;
+//   int result = 0;
+//   result += cnt * first;
+//   result += (m - cnt) * second;
 
-  cout << result << endl;
-}
+//   cout << result << endl;
+// }
