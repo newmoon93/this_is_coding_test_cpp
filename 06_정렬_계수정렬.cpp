1ef5c1bba0047bd64f1c752d34d697fdd69b4f81
newmoon93 <seongwon93@gmail.com>
2021-01-24 10:21:55 +0000

    sorting practice 6

---
+++ 06_정렬_계수정렬.cpp
@@ -0,0 +1,21 @@
+// #include <bits/stdc++.h>
+// #define MAX_VALUE 9
+
+// using namespace std;
+
+// int n = 15;
+
+// int arr[15] = {4,7,7,6,2,8,9,3,1,0,5,3,2,8,6};
+
+// int cnt[MAX_VALUE + 1];
+
+// int main(void) {
+//   for (int i = 0; i < n; i++) {
+//     cnt[arr[i]] += 1;
+//   }
+//   for (int i = 0; i <= MAX_VALUE; i++) {
+//     for (int j = 0; j < cnt[i]; j++) {
+//       cout << i << ' ';
+//     }
+//   }
+// }
