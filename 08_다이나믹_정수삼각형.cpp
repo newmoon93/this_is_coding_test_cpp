91a012e5a93d06550084813975c209fab64c618b
newmoon93 <seongwon93@gmail.com>
2021-01-31 15:33:10 +0000

    dynamic practice

---
+++ 08_다이나믹_정수삼각형.cpp
@@ -0,0 +1,33 @@
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int n;
+// int dp[500][500];
+// int main(void) {
+//     cin >> n;
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < i + 1; j++) {
+//             cin >> dp[i][j];
+//         }
+//     }
+    
+//     for (int i = 1; i < n; i++) {
+//         for (int j = 0; j <= i; j++) {
+//             int upLeft, up;
+
+//             if (j == 0) upLeft = 0;
+//             else upLeft = dp[i - 1][j - 1];
+
+//             if (j == i) up = 0;
+//             else up = dp[i - 1][j];
+
+//             dp[i][j] = dp[i][j] + max(upLeft, up);
+//         }
+//     }
+//     int result = 0;
+//     for (int i = 0; i < n; i++) {
+//         result = max(result, dp[n - 1][i]);
+//     }
+//     cout << result << '\n';
+// }
