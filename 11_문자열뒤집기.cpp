402212b33230e58470108b4518ea751c4b04c0e1
newmoon93 <seongwon93@gmail.com>
2021-02-07 16:11:41 +0000

    greedy_practice

---
+++ 11_문자열뒤집기.cpp
@@ -0,0 +1,31 @@
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// string str;
+// int count0 = 0; 
+// int count1 = 0;
+
+// int main(void) {
+//     cin >> str;
+
+    
+//     if (str[0] == '1') {
+//         count0 += 1;
+//     }
+//     else {
+//         count1 += 1;
+//     }
+
+    
+//     for (int i = 0; i < str.size() - 1; i++) {
+//         if (str[i] != str[i + 1]) {
+            
+//             if (str[i + 1] == '1') count0 += 1;
+            
+//             else count1 += 1;
+//         }
+//     }
+
+//     cout << min(count0, count1) << '\n';
+// }
