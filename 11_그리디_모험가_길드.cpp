2d5ab17bbca18845e32120718da39548ae7df3db
newmoon93 <seongwon93@gmail.com>
2021-02-07 14:04:11 +0000

    greedy_practice

---
+++ 11_그리디_모험가_길드.cpp
@@ -0,0 +1,31 @@
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int n;
+// vector<int> arr;
+
+// int main(void) {
+//     cin >> n;
+
+//     for (int i = 0; i < n; i++) {
+//         int x;
+//         cin >> x;
+//         arr.push_back(x);
+//     }
+
+//     sort(arr.begin(), arr.end());
+
+//     int result = 0;
+//     int count = 0;
+
+//     for (int i = 0; i < n; i++) {
+//         count += 1;
+//         if (count >= arr[i]) {
+//             result += 1;
+//             count = 0;
+//         }
+//     }
+
+//     cout << result << '\n';
+// }
